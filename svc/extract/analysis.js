56a1e6257394456eca9a6c4616d4fe8c37fe6bb6
aschmach <anders.schmachtenberg@gmail.com>
2020-05-16 11:04:07 +1000

    handling cases where there is no date

--- svc/extract/analysis.js
+++ svc/extract/analysis.js
@@ -1,4 +1,5 @@
 const unfluff = require("unfluff");
+const moment = require("moment-timezone");
 
 const { analyseContent } = require("./analyseContent");
 const { analyseLinks } = require("./analyseLinks");
@@ -31,13 +32,28 @@ async function analysis(content_url, data, frame_compatible) {
   if (canonicalLink) content_url = canonicalLink;
 
   // if (!date) date = "";
-  const dateConfig = {
-    target: "helper-date",
-    data: { date, url: content_url },
-    trigger: "scanner",
-    mins: 1,
-  };
-  const publication_date = await postData(dateConfig);
+  let publication_date = moment().tz("America/Chicago").toISOString();
+
+  if (date) {
+    const dateConfig = {
+      target: "helper-date",
+      data: { date, url: content_url },
+      trigger: "scanner",
+      mins: 1,
+    };
+    const date_response = await postData(dateConfig);
+    if (data_response) {
+      publication_date = date_response;
+    } else {
+      console.log(
+        `no proper date response for ${content_url}, reverting to default`
+      );
+    }
+  } else {
+    console.log(
+      `no date from unfluff for ${content_url}, reverting to default`
+    );
+  }
   const domain = getDomain(content_url);
   const amp_url = getSpecificUrl(data, "amphtml");
 
